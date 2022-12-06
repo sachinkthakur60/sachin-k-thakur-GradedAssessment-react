f8eb17a20a06ce9a6d2779711b2622c0bce70c85
sachinkthakur60 <sachinkthakur60@yahoo.com>
2022-12-06 14:28:22 +0530

    GradedAssessment-react

---
+++ GradedAssessment-react/client/src/components/NavBar.tsx
@@ -0,0 +1,46 @@
+import React, {Fragment, useState} from 'react';
+import {HiSearch} from 'react-icons/hi';
+import "../styles/NavBarStyles.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Movies from './Movies';
+import MovieInfo from './MovieInfo';
+
+function NavBar(){
+    const [search, setSearch]= useState("");
+    return (
+        <Fragment>
+            <Router>
+            <nav className="navigate-tabs">
+                <div className='nav-options'>
+                    <Link to={''} state ={{tab: "movies-in-theaters"}}>
+                    <span>Movies in Theater</span>
+                    </Link>
+                    <Link to={''} state= {{ tab: "movies-coming" }} >
+                    <span> Coming Soon</span>
+                    </Link>
+                    <Link to={''} state= {{ tab: "top-rated-india" }} >
+                    <span> Top rated Indian</span>
+                    </Link>
+                    <Link to={''} state= {{ tab: "top-rated-movies" }} >
+                    <span> Top rated </span>
+                    </Link>
+                    <Link to={''} state= {{ tab: "favourit" }} >
+                    <span> Favourites </span>
+                    </Link>
+                </div>
+                <div className='input-group'>
+                    <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)}/>
+                    <HiSearch color='green'/>
+                </div>
+            </nav>
+            
+                <Routes>
+                        <Route path="" element={<Movies searchValue={search} />} />
+                        <Route path="/:MovieName" element={<MovieInfo />} />
+                </Routes>
+            </Router>
+        </Fragment>
+    );
+}
+
+export default NavBar
---
+++ GradedAssessment-react/client/src/services/movies.tsx
@@ -0,0 +1,49 @@
+import axios from "axios"
+
+export const getMoviesFromServer = async(movieListType : string, search : string) => {
+    console.log("test")
+    console.log(search)
+    if ( search === undefined ||  search === '')
+    {
+        const Api = "http://localhost:4001/" + movieListType
+        return await axios.get(Api).then(res => res.data)
+    }
+    const Api = "http://localhost:4001/" + movieListType + "?title_like=" + search
+    return await axios.get(Api).then(res => res.data)
+}
+
+export const addMovieToFavouritesInServer = (movie: any) => {
+    return axios.post(
+        `http://localhost:4001/favourit`,
+        movie,
+        {
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }
+    ).then( response => response.status )
+    
+}
+
+export const removeMovieFromFavouritesInServer = (movieId: any) => {
+    const Api = "http://localhost:4001/favourit/" + movieId
+    return axios.delete(Api).then( response => response.status )
+}
+
+
+export const getMovieFavourites = (movieName=undefined) => {
+    if(movieName){
+        const Api = "http://localhost:4001/favourit?title=" + movieName
+        return axios.get(Api).then( response => response.data )
+    }
+    return axios.get(`http://localhost:4001/favourit`).then( response => response.data )
+}
+
+export const getMovies = (tab=undefined, movieName=undefined) => {
+    if(movieName){
+        const Api = "http://localhost:4001/" + tab + "?title=" + movieName
+        return axios.get(Api).then( response => response.data )
+    }
+    const Api = "http://localhost:4001/" + tab 
+    return axios.get(Api).then( response => response.data )
+}
